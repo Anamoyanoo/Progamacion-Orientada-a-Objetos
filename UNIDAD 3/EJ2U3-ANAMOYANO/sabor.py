ab4032a13be8b83a809ff792138cb5bacdd44ac0
Ana Moyano <89810046+Anamoyanoo@users.noreply.github.com>
2023-06-15 12:28:11 -0300

    Delete UNIDAD 3 directory

--- UNIDAD 3/EJ2U3-ANAMOYANO/sabor.py
+++
@@ -1,9 +0,0 @@
-class Sabor(object):
-    def __init__(self, idsabor, ingredientes, nombre):
-        self.__idsabor=idsabor
-        self.__ingredientes=ingredientes
-        self.__nombre=nombre
-    def __str__(self):
-        return f"Nombre: {self.__nombre} - ID: {self.__idsabor}"
-    def id(self):
-        return self.__idsabor
--- UNIDAD 3/EJ3U3-ANAMOYANO/persona.py
+++
@@ -1,5 +0,0 @@
-class Persona(object):
-	def __init__(self,nombre,direccion,dni):
-		self.__nombre = nombre
-		self.__direccion = direccion
-		self.__dni = dni
